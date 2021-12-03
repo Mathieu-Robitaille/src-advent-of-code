937ddaaafe43e7166859797b0c0c6f09d22a4181
mrobitaille <mrobitaille@stingray.com>
2021-12-03 10:28:02 -0500

    Fixing a dumb

--- src/python/2021/01/cursed/part1.py
+++ src/python/2021/01/cursed/part1.py
@@ -5,6 +5,6 @@ from itertools import tee, islice
 
 # Quality of life, define the input file location
 src = Path(modules['__main__'].__file__).resolve().parent
-input_file_path = Path(src, "input.txt")
+input_file_path = Path(src, "..", "input.txt")
 
 print("The number of measurements is : ", len([a for a, b in list(zip(*[islice(open(input_file_path, "r").read().split(), x, None) for x in range(2)])) if int(b) > int(a)]))
