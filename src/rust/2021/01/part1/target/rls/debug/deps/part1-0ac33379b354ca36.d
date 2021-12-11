0729797fda8b6ff7e161483690b34b9dbfdb813e
mrobitaille <mrobitaille@stingray.com>
2021-12-11 09:48:09 -0500

    What in tarnation is target doin in here?

--- src/rust/2021/01/part1/target/rls/debug/deps/part1-0ac33379b354ca36.d
+++
@@ -1,6 +0,0 @@
-/home/jm/git/src-advent-of-code/src/rust/2021/01/part1/target/rls/debug/deps/part1-0ac33379b354ca36.rmeta: src/main.rs src/../../input.txt
-
-/home/jm/git/src-advent-of-code/src/rust/2021/01/part1/target/rls/debug/deps/part1-0ac33379b354ca36.d: src/main.rs src/../../input.txt
-
-src/main.rs:
-src/../../input.txt:
--- src/rust/2021/01/part2/target/rls/debug/deps/part2-ed7fd6e5d48257c8.d
+++
@@ -1,6 +0,0 @@
-/home/jm/git/src-advent-of-code/src/rust/2021/01/part2/target/rls/debug/deps/part2-ed7fd6e5d48257c8.rmeta: src/main.rs src/../../input.txt
-
-/home/jm/git/src-advent-of-code/src/rust/2021/01/part2/target/rls/debug/deps/part2-ed7fd6e5d48257c8.d: src/main.rs src/../../input.txt
-
-src/main.rs:
-src/../../input.txt:
