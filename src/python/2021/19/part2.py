4dd095c816e8d0cf081c3ca7b33318baf051442c
mrobitaille <mrobitaille@stingray.com>
2021-12-20 16:52:55 -0500

    Day 20! oh boy these are getting less fun...

--- src/python/2021/19/part2.py
+++ src/python/2021/19/part2.py
@@ -6,8 +6,5 @@ from sys import modules
 src = Path(modules['__main__'].__file__).resolve().parent
 input_file_path = Path(src, "input.txt")
 
-test_input = "target area: x=20..30, y=-10..-5"
-
-# with open(input_file_path) as f:
-#     pass
-
+with open(input_file_path) as f:
+    pass
